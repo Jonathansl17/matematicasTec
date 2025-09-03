09ada8db3a94e56ce4ad09b5321f38df4d7d1f6d
Jonathansl17 <j.sancho.1@estudiantec.cr>
2025-09-03 16:14:08 -0600

    Normalizacion de nombres

---
+++ probabilidades/PrimerParcial/tareas/calculos.py
@@ -0,0 +1,15 @@
+
+import math
+
+universo = math.comb(35,12)
+
+caso1 = math.comb(17,6)*math.comb(18,6)
+caso2 = math.comb(17,5)*math.comb(18,7)
+caso3 = math.comb(17,7)*math.comb(18,5)
+
+
+resultado = caso1+caso2+caso3
+
+print(resultado)
+
+
